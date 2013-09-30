625f7c08de5aa0686f555ac7bdc29e6395b81b5a
Tony Burleson <tjbgatari@yahoo.com>
2013-09-29 17:06:13 -1000

    testy testy

--- learnGradle/src/com/company/anime/AnimeAction.java
+++ learnGradle/src/com/company/anime/AnimeAction.java
@@ -1,14 +1,23 @@
 package com.company.anime;
 
-public class AnimeAction {
+public class AnimeAction
+{
+
+	private String name;
 
 	public void yo()
 	{
 		System.out.println("yo");
 	}
-	
+
 	public String getName()
 	{
-		return "derf";
+		return name;
 	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
 }
